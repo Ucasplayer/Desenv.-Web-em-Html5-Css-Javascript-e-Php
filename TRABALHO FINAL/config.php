e33fb0b5626ea6093a0c9d3a7c5e73673f711234
Ucasplayer <90428029+Ucasplayer@users.noreply.github.com>
2021-11-30 20:19:04 -0300

    Add files via upload

---
+++ TRABALHO FINAL/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = 'root';
+$dbname = 'Formulario trabalho';
+
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword, $dbname)
+
+
+?>
